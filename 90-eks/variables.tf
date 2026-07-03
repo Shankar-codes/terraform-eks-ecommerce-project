35ede3fed5371c9ac0dcc1acc7770c17c0df4d80
Shankar-codes <harikainteriors@gmail.com>
2026-07-03 12:33:32 +0530

    eks creation

---
+++ 90-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "ellamma-roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z00405822WO8HCQACP58A"
+}
+
+variable "domain_name" {
+  default     = "ellamma.fun"
+}
