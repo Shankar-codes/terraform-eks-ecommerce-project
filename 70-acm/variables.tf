79fe3117276322ca45094d6245a1166cf9517217
Shankar-codes <harikainteriors@gmail.com>
2026-07-03 12:31:30 +0530

    certificate manager

---
+++ 70-acm/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default = "ellamma-roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "ami_id" {
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "zone_id" {
+  default = "Z00405822WO8HCQACP58A"
+}
+
+variable "domain_name" {
+  default     = "ellamma.fun"
+}
