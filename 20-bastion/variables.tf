25b933909be92856338e7e63658db81376653bbe
Shankar-codes <harikainteriors@gmail.com>
2026-07-03 12:30:04 +0530

    security group

---
+++ 20-bastion/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "ellamma-roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "ami_id" {
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
